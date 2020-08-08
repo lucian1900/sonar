4ad55094c747988da27f701e7bdd965ff3fe0286
Luci <luci@Lucis-Digit-laptop.local>
2020-08-08 18:00:56 +0100

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var _a;
-import { Howl } from "howler";
+import {Howl} from "./web_modules/howler.js";
+console.log(Howl);
 const sound = new Howl({
-    src: ["sonar.wav"],
+  src: ["/assets/sonar.wav"]
 });
 const sonar = () => {
-    console.log('sonar!');
-    sound.play();
+  console.log("sonar!");
+  sound.play();
 };
-(_a = document.getElementById("sound")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", sonar);
+document.getElementById("sound")?.addEventListener("click", sonar);
 //# sourceMappingURL=index.js.map
