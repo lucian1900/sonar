bdc60bef63af62a867c540b361decf8ca55978e9
Luci <luci@Lucis-Digit-laptop.local>
2020-08-08 12:32:50 +0100

    wav import without errors.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { Howl } from "howler"
 
 const wavs = require("../static/*.wav");
 
-//import wavs from "../static/*.wav";
-
 console.log(wavs)
 const sound = new Howl({
     src: [wavs.sonar],
