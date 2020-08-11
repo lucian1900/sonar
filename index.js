d3fe0be5096ef1a9517234a11f01e7e327011ce7
Luci <luci@Lucis-Digit-laptop.local>
2020-08-11 23:01:21 +0100

    Updates

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import howler2 from "./web_modules/howler.js";
+import nipple from "./web_modules/nipplejs.js";
 const sound = new howler2.Howl({
-  src: ["./assets/sonar.wav"],
+  src: ["./assets/sonar.webm", "./assets/sonar.mp3"],
   loop: true
 });
-let DIST = 5;
-let x = DIST;
-let y = 0;
 sound.play();
-sound.on("end", () => {
-  if (x > 0) {
-    x = -DIST;
-  } else {
-    x = DIST;
-  }
-  sound.pos(x, y, 0);
+const joystick = nipple.create({
+  zone: document.querySelector("#joystick"),
+  color: "purple",
+  mode: "static",
+  position: {
+    left: "50%",
+    top: "50%"
+  },
+  restJoystick: false,
+  lockX: true
+});
+joystick.on("move", (_, data) => {
+  sound.pos(data.vector.x, 0, 0);
 });
 //# sourceMappingURL=index.js.map
